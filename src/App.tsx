05583cada5323565b29f83870bb38a9214e804e9
msenador <smorian@yahoo.com>
2023-10-05 13:29:18 -0400

    basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+export interface User {
+  _id: string;
+  name: string;
+  eyes: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>();
+
+  const initDb = async () => {
+    const response = await fetch(
+      "https://techop-rpa-demo-functions.azurewebsites.net/api/init-db?code=FW8mFHW6aZIZwKC-DwAoxslckCbNw-5vlLGCfffvtWVdAzFuLfLZ-g==",
+      {}
+    );
+    const json = await response.json();
+    console.log("J: ", json);
+    setUsers(json);
+  };
+
+  useEffect(() => {
+    initDb();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Test ing<code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,6 +41,14 @@ function App() {
           Learn React
         </a>
       </header>
+      {users?.map((user) => {
+        return (
+          <div>
+            <div>Name: {user.name}</div>
+            <li>Eyes: {user.eyes}</li>
+          </div>
+        );
+      })}
     </div>
   );
 }
